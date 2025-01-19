043e35515d24580be6513f17be74a31a4b9436b8
Yoooung23 <logan7design@gmail.com>
2025-01-18 19:57:44 -0700

    new js

--- mission/mission.js
+++ mission/mission.js
@@ -2,10 +2,10 @@ const themeSelector = document.querySelector("#theme-select")
 function changeTheme() {
     if (themeSelector.value === "dark") {
         document.body.classList.add("dark-mode")
-        document.getElementById('logo').src = 'mission/images/byui-logo_white.png';
-    } else {
-        document.body.classList.remove("dark-mode")
-        document.getElementById('logo').src = 'images/byui-logo_blue.webp';
+        document.getElementById('logo').src = 'images/byui-logo_white.png';
+    } else { 
+        document.body.classList.remove("dark-mode");
+        document.getElementById('logo').src = 'images/byui-logo_blue.webp'; 
     }
 
 }
