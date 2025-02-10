6fa0639c8b393ef28f87f90a6c071cbc4b87e580
Yoooung23 <logan7design@gmail.com>
2025-02-10 09:07:18 -0700

    new page

---
+++ blog/review.js
@@ -0,0 +1,27 @@
+const articles = [
+	{
+		id: 1,
+		title: 'Fablehaven Book 1',
+		date: 'June 14, 2006',
+		description:
+			'None who enter will leave unchanged.',
+		imgSrc: 'https://m.media-amazon.com/images/I/81-CHrH4mCL._SL1500_.jpg',
+		imgAlt: 'Book cover for Fablehaven Book 1',
+		ages: '6-12',
+		genre: 'Fantasy',
+		stars: '⭐⭐⭐⭐⭐'
+	},
+	{
+		id: 2,
+		title: 'The Lightning Thief',
+		date: 'December 12, 2021',
+		description:
+			'The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.',
+		imgSrc:
+			'https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300',
+		imgAlt: 'Book cover for Magnus Chase 1',
+		ages: '12-16',
+		genre: 'Fantasy',
+		stars: '⭐⭐⭐⭐'
+	}
+]
